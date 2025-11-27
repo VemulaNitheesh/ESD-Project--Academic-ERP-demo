e13005274f08d6ce5528d5f487039f005d241362
VemulaNitheesh <vemulanitheesh05@gmail.com>
2025-11-27 23:58:17 +0530

    SQL

---
+++ Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO Students (ID, Email, Name, Roll_Number, Department_ID) VALUES
+(1, 'rohit.cs@iiitb.ac.in', 'Rohit Sharma', 'MT2025001', 1),
+(2, 'ananya.cs@iiitb.ac.in', 'Ananya Gupta', 'MT2025002', 1),
+(3, 'vijay.se@iiitb.ac.in', 'Vijay Kumar', 'MT2025400', 2),
+(4, 'nikita.se@iiitb.ac.in', 'Nikita Raj', 'MT2025401', 2),
+(5, 'arjun.ee@iiitb.ac.in', 'Arjun Singh', 'MT2025700', 3),
+(6, 'sana.ee@iiitb.ac.in', 'Sana Mirza', 'MT2025701', 3);
+
+INSERT INTO domain (domain_id, domain_name)
+VALUES
+(1, 'Computer Science'),
+(2, 'Software Engineering'),
+(3, 'Electrical Engineering');
